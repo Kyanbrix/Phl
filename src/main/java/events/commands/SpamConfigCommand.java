830f1e70ac868af74f8749aff53be90e399cd8f4
Kian <kyanbrix@yahoo.com>
2025-01-15 09:39:18 +0800

    Added a Spam Configuration Command

--- src/main/java/events/commands/SpamConfigCommand.java
+++ src/main/java/events/commands/SpamConfigCommand.java
@@ -1,2 +1,43 @@
-package events.commands;public class SpamConfigCommand {
+package events.commands;
+
+import com.github.kyanbrix.Main;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import utilities.Constant;
+
+public class SpamConfigCommand extends Command{
+    @Override
+    public void execute(MessageReceivedEvent event) {
+
+
+        String configMsg = removePrefix(event.getMessage().getContentRaw());
+
+        if (configMsg.equalsIgnoreCase("e") || configMsg.equalsIgnoreCase("enable")) {
+
+            if (!Main.getInstance().getSpying()) {
+                Main.getInstance().setSpying(true);
+                sendMessage(event.getChannel(),"Anti-Spamming is Enabled!");
+            }else sendMessage(event.getChannel(),"Anti-Spamming Configuration is already Enabled!");
+
+
+
+        } else if (configMsg.equalsIgnoreCase("d") || configMsg.equalsIgnoreCase("disable")) {
+            if (!Main.getInstance().getSpying()) {
+                Main.getInstance().setSpying(false);
+                sendMessage(event.getChannel(),"Anti-Spamming is Disabled!");
+            }else sendMessage(event.getChannel(),"Anti-Spamming Configuration is already Disabled!");
+        }
+
+
+    }
+
+    @Override
+    public String getName() {
+        return "antispam";
+    }
+
+    private String removePrefix(String message) {
+
+        return message.substring(Constant.prefix.length() + getName().length()).strip();
+
+    }
 }
